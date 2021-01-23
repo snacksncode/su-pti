b9c657d1d59f44fa90e4dff5803ca061dcf1602e
Filip Kania <fkania2@gmail.com>
2021-01-23 13:37:16 +0100

    Team: finished desktop version

--- components/Team/Team.component.tsx
+++ components/Team/Team.component.tsx
@@ -1,25 +1,29 @@
 import Container from "../Container";
 import ProgressiveImage from 'react-progressive-graceful-image';
+import classNames from 'classnames';
 import styles from "./Team.module.scss";
 
 const folks = [
   {
-    name: "Maximilian Cyprys",
-    img: "https://source.unsplash.com/random/250x250/?nature,fog,forest",
-    role: "Skarbnik",
-    desc: "Lorem ipsum dolor sit amet, adsfasdfasdfasdfasfasfadfasdfafadfadfasdfafdasfasdfasdfsdafafasdfadfadfasdfadsf"//consectetur adipiscing elit. Pellentesque odio augue, cursus sed ipsum sit amet, condimentum scelerisque tortor. Mauris euismod purus nisi, et vulputate sapien tempor a."
+    name: "Patryk Kulec",
+    img: "https://source.unsplash.com/random/250x250/?nature,fog",
+    role: "Przewodniczący",
+    isLeader: true,
+    desc: "Lorem ipsum dolor sit amet."
   },
   {
     name: "Maximilian Cyprys",
-    img: "https://source.unsplash.com/random/250x250/?nature,fog,forest",
+    img: "https://source.unsplash.com/random/250x250/?nature,forest",
     role: "Skarbnik",
-    desc: "Lorem ipsum dolor sit amet, adsfasdfasdfasdfasfasfadfasdfafadfadfasdfafdasfasdfasdfsdafafasdfadfadfasdfadsf"//consectetur adipiscing elit. Pellentesque odio augue, cursus sed ipsum sit amet, condimentum scelerisque tortor. Mauris euismod purus nisi, et vulputate sapien tempor a."
+    isLeader: false,
+    desc: "Lorem ipsum dolor sit amet."
   },
   {
-    name: "Maximilian Cyprys",
-    img: "https://source.unsplash.com/random/250x250/?nature,fog,forest",
-    role: "Skarbnik",
-    desc: "Lorem ipsum dolor sit amet, adsfasdfasdfasdfasfasfadfasdfafadfadfasdfafdasfasdfasdfsdafafasdfadfadfasdfadsf"//consectetur adipiscing elit. Pellentesque odio augue, cursus sed ipsum sit amet, condimentum scelerisque tortor. Mauris euismod purus nisi, et vulputate sapien tempor a."
+    name: "Szymon Babik",
+    img: "https://source.unsplash.com/random/250x250/?fog,forest",
+    role: "Zastępca",
+    isLeader: false,
+    desc: "Lorem ipsum dolor sit amet."
   }
 ]
 
@@ -32,7 +36,7 @@ const Team = () => {
 
       <div className={styles.container}>
         {folks.map((person, i) => (
-          <div key={i} className={styles.card}>
+          <div key={i} className={classNames(styles.card, (person.isLeader ? styles.leader : ""))}>
             <ProgressiveImage src={person.img} placeholder="">
               {(src, loading) => (
                   <div className={styles.imageContainer}>
@@ -41,7 +45,7 @@ const Team = () => {
                   </div>
                 )}
             </ProgressiveImage>
-            
+
             <div className={styles.infoContainer}>
               <span className={styles.name}>{person.name}</span>
               <span className={styles.role}>{person.role}</span>

413fa38e35cb8292fbe400a7e48e7c67a776c795
Filip Kania <fkania2@gmail.com>
2021-01-23 12:58:29 +0100

    Team: fixed some styling issues

--- components/Team/Team.component.tsx
+++ components/Team/Team.component.tsx
@@ -7,7 +7,19 @@ const folks = [
     name: "Maximilian Cyprys",
     img: "https://source.unsplash.com/random/250x250/?nature,fog,forest",
     role: "Skarbnik",
-    desc: "Lorem ipsum dolor sit amet, "//consectetur adipiscing elit. Pellentesque odio augue, cursus sed ipsum sit amet, condimentum scelerisque tortor. Mauris euismod purus nisi, et vulputate sapien tempor a."
+    desc: "Lorem ipsum dolor sit amet, adsfasdfasdfasdfasfasfadfasdfafadfadfasdfafdasfasdfasdfsdafafasdfadfadfasdfadsf"//consectetur adipiscing elit. Pellentesque odio augue, cursus sed ipsum sit amet, condimentum scelerisque tortor. Mauris euismod purus nisi, et vulputate sapien tempor a."
+  },
+  {
+    name: "Maximilian Cyprys",
+    img: "https://source.unsplash.com/random/250x250/?nature,fog,forest",
+    role: "Skarbnik",
+    desc: "Lorem ipsum dolor sit amet, adsfasdfasdfasdfasfasfadfasdfafadfadfasdfafdasfasdfasdfsdafafasdfadfadfasdfadsf"//consectetur adipiscing elit. Pellentesque odio augue, cursus sed ipsum sit amet, condimentum scelerisque tortor. Mauris euismod purus nisi, et vulputate sapien tempor a."
+  },
+  {
+    name: "Maximilian Cyprys",
+    img: "https://source.unsplash.com/random/250x250/?nature,fog,forest",
+    role: "Skarbnik",
+    desc: "Lorem ipsum dolor sit amet, adsfasdfasdfasdfasfasfadfasdfafadfadfasdfafdasfasdfasdfsdafafasdfadfadfasdfadsf"//consectetur adipiscing elit. Pellentesque odio augue, cursus sed ipsum sit amet, condimentum scelerisque tortor. Mauris euismod purus nisi, et vulputate sapien tempor a."
   }
 ]
 
@@ -19,19 +31,18 @@ const Team = () => {
       </div>
 
       <div className={styles.container}>
-        {folks.map(person => (
-          <div className={styles.card}>
-            <div>
-              <ProgressiveImage src={person.img} placeholder="">
-                {(src, loading) => (
-                    <div>
-                      <img src={src}/>
-                      <div className={styles.imagePlaceholder} style={{ opacity: loading ? 1 : 0 }}/>
-                    </div>
-                  )}
-              </ProgressiveImage>
-            </div>
-            <div>
+        {folks.map((person, i) => (
+          <div key={i} className={styles.card}>
+            <ProgressiveImage src={person.img} placeholder="">
+              {(src, loading) => (
+                  <div className={styles.imageContainer}>
+                    <img className={styles.img} src={src}/>
+                    <div className={styles.placeholder} style={{ opacity: loading ? 1 : 0 }}/>
+                  </div>
+                )}
+            </ProgressiveImage>
+            
+            <div className={styles.infoContainer}>
               <span className={styles.name}>{person.name}</span>
               <span className={styles.role}>{person.role}</span>
               <span className={styles.desc}>{person.desc}</span>

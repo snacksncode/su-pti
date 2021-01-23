0cde096694e85a92dd45b75c3313340bea2c7756
Filip Kania <fkania2@gmail.com>
2021-01-23 10:30:39 +0100

    Hero: mobile is ready

--- components/Hero/Hero.component.tsx
+++ components/Hero/Hero.component.tsx
@@ -1,6 +1,8 @@
 import styles from "./Hero.module.scss";
 import Container from '../Container';
 
+import classNames from 'classnames';
+
 const imgUrl = "https://images.unsplash.com/photo-1609612006487-e6211790ece5?crop=entropy&fit=crop&fm=jpg&h=1080&q=80&w=1920";
 
 const Hero = () => {
@@ -15,6 +17,16 @@ const Hero = () => {
           <div className={styles.welcomeText}>Witamy na stronie</div>
           <div className={styles.govText}>Samorządu</div>
           <div className={styles.studentText}>Uczniowskiego</div>
+        </div>
+
+        <div className={styles.buttonContainer}>
+          <button className={styles.button}>
+            Zobacz skład
+          </button>
+
+          <button className={classNames(styles.button, styles.secondaryButton)}>
+            Szczęśliwy numerek
+          </button>
         </div>
       </Container>
   	</div>

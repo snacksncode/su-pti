e4e8852f225f9929695485c241e9c7d6a6533430
Filip Kania <fkania2@gmail.com>
2021-01-24 12:47:14 +0100

    Fix: added types to react-scroll

--- components/Scroll/Scroll.component.tsx
+++ components/Scroll/Scroll.component.tsx
@@ -7,7 +7,7 @@ const Scroll = ({ children, to, withOffset = false, onClick }: {
     withOffset?: boolean;
     onClick?: () => void;
 }) => (
-    <ScrollLink active={undefined} onClick={onClick} to={to} offset={withOffset ? -65 : 0} smooth={"easeInOutCubic"} spy={true}>
+    <ScrollLink onClick={onClick} to={to} offset={withOffset ? -65 : 0} smooth={"easeInOutCubic"} spy={true}>
         {children}
     </ScrollLink>
 )
